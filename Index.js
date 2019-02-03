154e1f4c14affa14264a1ecedb88cb709ade3312
Brian Lui <inaspanishway@gmail.com>
2019-02-03 15:46:42 +0800

    what

--- Index.js
+++ Index.js
@@ -1,6 +1,6 @@
-import Particles from './Particles';
-import Player from './Player';
-import preloadAll from './preload';
+import Particles from './Particles.js';
+import Player from './Player.js';
+import preloadAll from './preload.js';
 
 function preload ()
 {
