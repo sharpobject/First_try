2433773b3ea971a05b154f1fc9cf80cba09def8c
Brian Lui <inaspanishway@gmail.com>
2019-02-07 16:53:47 +0800

    sorry

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,19 @@
 const presets = [
-  "@babel/typescript",
+    [
+        "@babel/typescript",
+        {
+            "targets": {
+                "browsers": ["last 2 versions", "safari >= 7"],
+                "node": "6.10"
+            },
+        },
+    ],
 ];
 
 const plugins = [
-  "@babel/proposal-class-properties",
-  "@babel/proposal-object-rest-spread",
+    "@babel/proposal-class-properties",
+    "@babel/proposal-object-rest-spread",
 ]
 
 module.exports = { presets, plugins };
+
